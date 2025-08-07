f13d1370c82af5fabbc60a8d135b9d049bdc1ee5
Luana Duarte <luanavduarte02@gmail.com>
2025-08-07 19:59:21 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int frequencia;
+        String nome;
+        
+        public Estudante( int frequencia, string nome) {
+            this.nome = nome;
+            this.frequencia = frequencia;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Estudante[] estudantes = new Estudantes[5];
+        estudantes[0] = new Estudante("breno", 6);
+        Estudantes[1] = new Estudante("luana", 100 );
+        estudantes[2] = new Estudante("Nicolle", 20);
+        estudantes[3] = new Estudante("clara", 33);
+        estudantes[4] = new Estudante("patricia",2);
+        
+        int frequencia = 0;
+        
+        System.out.println("frequencia acima de 100");
+        
+        for(Estudante estudantes : estudantes) { 
+            if (estudantes.frequencia < 100) {
+                System.out.println(estudantes.nome + ":" + frequencia + 100);
+                
+            }
+            
+            somafrequencia+= frequencia
+           count ++;
+    }
+   double frequencia = (double) somafrequencia / count
+   System.out.println("frequencia acima de 100")
+    }
+}
+        
